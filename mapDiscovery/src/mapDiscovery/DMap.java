92afb5c3d4fc12bc04ff323dc09d8cf85b0900c9
Hazardius <hazardiusam@gmail.com>
2014-05-29 01:49:27 +0200

    Adjustments. TODO: Check on real robot.

--- mapDiscovery/src/mapDiscovery/DMap.java
+++ mapDiscovery/src/mapDiscovery/DMap.java
@@ -3,91 +3,264 @@ package mapDiscovery;
 import lejos.nxt.LCD;
 
 public class DMap {
-	private char[][] mapData;
-	private int xDim, yDim;
-	
+    private char[][] mapData;
+    private int xDim, yDim;
+    private boolean sSet, cSet, nSet;
+    
 
     /*n - pole nieznane,
     S – pole startowe,
     B – pole bia³e,
     C – czerwone pole,
-    N – niebieskie pole,
-    P – niebieska pi³eczka,
-    H – czerwony szeœcian.*/
-	
-	public DMap() {
-		this(4, 4);
-	}
-	
-	public DMap(int x, int y) {
-		xDim = x;
-		yDim = y;
-	    mapData = new char[x][y];
-	    for (int i = 0; i < xDim; i++) {
-	    	for (int j = 0; j < yDim; j++) {
-	    		mapData[i][j] = 'n';	// nil value
-	    	}
-	    }
-	}
-	
-	public void setField(int xPos, int yPos, char value) {
-		if ((xPos < xDim) && (yPos < yDim)) {
- 			mapData[xPos][yPos] = value;
-		}
-	}
-	
-	public void printOnDisplay() {
-		LCD.clear();
-	    for (int i = 0; i < xDim; i++) {
-	    	for (int j = 0; j < yDim; j++) {
-	            LCD.drawString("" + mapData[i][j], i,j);
-	    	}
-	    }
-	}
-	
-	private int heurestic_len(int one_x, int one_y, int two_x, int two_y) {
-		return (Math.abs(one_x - two_x)+Math.abs(one_y - two_y));
-	}
+    N – niebieskie pole.*/
+    
+    public DMap() {
+        this(4, 4);
+    }
+    
+    public DMap(int x, int y) {
+        xDim = x;
+        yDim = y;
+        mapData = new char[x][y];
+        for (int i = 0; i < xDim; i++) {
+            for (int j = 0; j < yDim; j++) {
+                mapData[i][j] = 'n';    // nil value
+            }
+        }
+    }
 
-	public String findAWay(int pos_x, int pos_y) {
-		String way = "";
-		int min = 7;
-		int[][] wybrane = new int[6][2];
-		int ch_co = 0;
-		for (int row=0; row < 4; row++) {
-			for (int col=0; col < 4; col++) {
-				int h_len = heurestic_len(row, col, pos_x, pos_y);
-				if (h_len == min) {
-					wybrane[ch_co][0] = row;
-					wybrane[ch_co][1] = col;
-					ch_co++;
-				} else if (h_len < min) {
-					wybrane = new int[5][2];
-					wybrane[0][0] = row;
-					wybrane[0][1] = col;
-					min = h_len;
-					ch_co = 1;
-				}
-			}
-		}
-		// W tym momencie mamy listê "najbli¿szych pól" w tabelce wybrane!
-		way = "lfffrf";
-		return way;
-	}
+    private void whiteAll() {
+        for (int i = 0; i < xDim; i++) {
+            for (int j = 0; j < yDim; j++) {
+                if (mapData[i][j] = 'n')
+                    mapData[i][j] = 'B';
+            }
+        }
+    }
+    
+    public void setField(int xPos, int yPos, char value) {
+        if ((xPos < xDim) && (yPos < yDim)) {
+            mapData[xPos][yPos] = value;
+            if (value == 'S') {
+                sSet = true;
+                cSet = false;
+                nSet = false;
+            }
+            if (value == 'C') cSet = true;
+            if (value == 'N') nSet = true;
+            if (sSet && cSet && nSet) whiteAll();
+        }
+    }
+    
+    public void printOnDisplay() {
+        LCD.clear();
+        for (int i = 0; i < xDim; i++) {
+            for (int j = 0; j < yDim; j++) {
+                LCD.drawString("" + mapData[i][j], i,j);
+            }
+        }
+    }
+    
+    private int heurestic_len(int one_x, int one_y, int two_x, int two_y) {
+        return (Math.abs(one_x - two_x) + Math.abs(one_y - two_y));
+    }
 
-	public boolean haveUnknown() {
-	    for (int i = 0; i < xDim; i++) {
-	    	for (int j = 0; j < yDim; j++) {
-	    		if (mapData[i][j] == 'n')
-	    			return true;
-	    	}
-	    }
-		return false;
-	}
+    public String wayTo(int pos_x, int pos_y, int jx, int jy, int orient) {
+        String way = "";
+        int cpx = pos_x;
+        int cpy = pos_y;
+        int cpo = orient;
+        while ((cpx != jx) || (cpy != jy)) {
+            switch (cpo) {
+                case 0: if (cpx == jx) {
+                            if (cpy < jy) {
+                                way += "f";
+                                cpy++;
+                            } else {
+                                way += "rr";
+                                cpo = 2;
+                            }
+                        } else if (cpy == jy) {
+                            if (cpx > jx) {
+                                way += "l";
+                                cpo = 3;
+                            } else {
+                                way += "r";
+                                cpo = 1;
+                            }
+                        } else {
+                            if (cpy < jy) {
+                                way += "f";
+                                cpy++;
+                            } else {
+                                if (cpx < jx) {
+                                    way += "r";
+                                    cpo = 1;
+                                } else {
+                                    way += "l";
+                                    cpo = 3;
+                                }
+                            }
+                        }
+                        break;
+                case 1: if (cpy == jy) {
+                            if (cpx < jx) {
+                                way += "f";
+                                cpx++;
+                            } else {
+                                way += "rr";
+                                cpo = 3;
+                            }
+                        } else if (cpx == jx) {
+                            if (cpy > jy) {
+                                way += "r";
+                                cpo = 2;
+                            } else {
+                                way += "l";
+                                cpo = 0;
+                            }
+                        } else {
+                            if (cpx < jx) {
+                                way += "f";
+                                cpx++;
+                            } else {
+                                if (cpy < jy) {
+                                    way += "l";
+                                    cpo = 0;
+                                } else {
+                                    wax += "r";
+                                    cpo = 2;
+                                }
+                            }
+                        }
+                        break;
+                case 2: if (cpx == jx) {
+                            if (cpy > jy) {
+                                way += "f";
+                                cpy++;
+                            } else {
+                                way += "rr";
+                                cpo = 0;
+                            }
+                        } else if (cpy == jy) {
+                            if (cpx > jx) {
+                                way += "r";
+                                cpo = 3;
+                            } else {
+                                way += "l";
+                                cpo = 1;
+                            }
+                        } else {
+                            if (cpy > jy) {
+                                way += "f";
+                                cpy++;
+                            } else {
+                                if (cpx < jx) {
+                                    way += "l";
+                                    cpo = 1;
+                                } else {
+                                    way += "r";
+                                    cpo = 3;
+                                }
+                            }
+                        }
+                        break;
+                case 3: if (cpy == jy) {
+                            if (cpx > jx) {
+                                way += "f";
+                                cpy++;
+                            } else {
+                                way += "rr";
+                                cpo = 1;
+                            }
+                        } else if (cpx == jx) {
+                            if (cpy > jy) {
+                                way += "l";
+                                cpo = 2;
+                            } else {
+                                way += "r";
+                                cpo = 0;
+                            }
+                        } else {
+                            if (cpx > jx) {
+                                way += "f";
+                                cpy++;
+                            } else {
+                                if (cpy < jy) {
+                                    way += "r";
+                                    cpo = 0;
+                                } else {
+                                    way += "l";
+                                    cpo = 2;
+                                }
+                            }
+                        }
+                        break;
+            };
+        };
+        return way;
+    }
 
-	public boolean isUnknown(int pos_x, int pos_y) {
-		if (mapData[pos_x][pos_y] == 'n')
-			return true;
-		return false;
-	}
+    public String findAWay(int pos_x, int pos_y, int orient) {
+        int min = 7;
+        int[][] wybrane = new int[6][2];
+        int ch_co = 0;
+        for (int row=0; row < 4; row++) {
+            for (int col=0; col < 4; col++) {
+                int h_len = heurestic_len(col, row, pos_x, pos_y);
+                if (h_len == min) {
+                    wybrane[ch_co][0] = col;
+                    wybrane[ch_co][1] = row;
+                    ch_co++;
+                } else if (h_len < min) {
+                    wybrane = new int[5][2];
+                    wybrane[0][0] = col;
+                    wybrane[0][1] = row;
+                    min = h_len;
+                    ch_co = 1;
+                }
+            }
+        }
+        int jx, jy;
+        int max = -1;
+        for (int wyb=0; wyb<ch_co; wyb++) {
+            int pv = 0;
+            if (wybrane[wyb][0] == pos_x) {
+                pv += 2;
+            } else if ((wybrane[wyb][0] < pos_x) && (orient == 3)) {
+                pv++;
+            } else if ((wybrane[wyb][0] > pos_x) && (orient == 1)) {
+                pv++;
+            }
+            if (wybrane[wyb][1] == pos_y) {
+                pv += 2;
+            } else if ((wybrane[wyb][1] < pos_y) && (orient == 2)) {
+                pv++;
+            } else if ((wybrane[wyb][1] > pos_y) && (orient == 0)) {
+                pv++;
+            }
+            if (max < pv) {
+                max = pv;
+                jx = wybrane[wyb][0];
+                jy = wybrane[wyb][1];
+            }
+        }
+        return wayTo(pos_x, pos_y, jx, jy, orient);
+    }
+
+    public boolean haveUnknown() {
+        for (int i = 0; i < xDim; i++) {
+            for (int j = 0; j < yDim; j++) {
+                if (mapData[i][j] == 'n')
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isUnknown(int pos_x, int pos_y) {
+        if (mapData[pos_x][pos_y] == 'n')
+            return true;
+        return false;
+    }
 }
--- mapDiscovery/src/mapDiscovery/Discovery.java
+++ mapDiscovery/src/mapDiscovery/Discovery.java
@@ -2,213 +2,212 @@ package mapDiscovery;
 
 import lejos.nxt.*;
 import lejos.robotics.localization.OdometryPoseProvider;
-import lejos.robotics.navigation.DifferentialPilot;
-import lejos.robotics.navigation.Pose;
-import lejos.robotics.objectdetection.Feature;
-import lejos.robotics.objectdetection.FeatureDetector;
-import lejos.robotics.objectdetection.RangeFeatureDetector;
+import lejos.robotics.navigation.*;
+import lejos.robotics.objectdetection.*;
 
 public class Discovery {
-	static int BLUE_L;
-	static int BLUE_H;
-	// BLACK is RED for MiniMapCre
-	static int RED_L;
-	static int RED_H;
-	static int WHITE_L;
-	static int WHITE_H;
-	
-	static int pos_x = -1;
-	static int pos_y = 3;
-	static int orient = 0; // 0-N, 1-E, 2-S, 3-W
+    static int BLUE_L;
+    static int BLUE_H;
+    // BLACK is RED for MiniMapCre
+    static int RED_L;
+    static int RED_H;
+    static int BLACK_L;
+    static int BLACK_H;
+    static int WHITE_L;
+    static int WHITE_H;
+    
+    static int pos_x = 3;
+    static int pos_y = -1;
+    static int orient = 0; // 0-N, 1-E, 2-S, 3-W
 
-	private static void setColorValues(LightSensor light) {
-		String kolory[] = {"niebieski", "czerwony", "bialy"}; 
-		int wartosci[] = new int[3];
-		for (int i=0; i<3; i++) {
-			LCD.clear();
-        	LCD.drawString("Pokaz " + kolory[i], 0, 0);
-        	LCD.drawString("I nacisnij prawy", 0, 1);
-		    while(!Button.RIGHT.isDown()) {
-				int value = light.getNormalizedLightValue();
-		        LCD.drawInt(value, 0,2);
-			}
-		    while(!Button.RIGHT.isUp()) {}
-			int value = light.getNormalizedLightValue();
-			wartosci[i] = value;
-		}
-		BLUE_L = wartosci[0] - 20;
-		int temp = wartosci[1] - wartosci[0];
-		if (temp < 30){
-			BLUE_H = wartosci[0] + (int)Math.floor(temp/2);
-			RED_L = BLUE_H;
-		} else {
-		    BLUE_H = wartosci[0] + 15;
-		    RED_L = wartosci[1] - 15;
-		}
-		temp = wartosci[2] - wartosci[1];
-		if (temp < 30){
-			RED_H = wartosci[1] + (int)Math.floor(temp/3);
-			WHITE_L = RED_H;
-		} else {
-		    RED_H = wartosci[1] + 15;
-		    WHITE_L = wartosci[2] - 15;
-		}
-		WHITE_H = wartosci[2] + 20;
-	}
-	
-	private static String color(int value) {
-		if ((value > BLUE_L)&&(value <= BLUE_H))
-			return "blue";
-		if ((value > RED_L)&&(value <= RED_H))
-			return "red";
-		if ((value > WHITE_L)&&(value <= WHITE_H))
-			return "white";
-		return "unknown";
-	}
-	
-	private static DMap moveOneFront(DifferentialPilot pilot, LightSensor light, DMap mapa) {
-		int new_val = light.getNormalizedLightValue();
-		String start_color = color(new_val);
-		int old_val = new_val;
-		String new_color = start_color;
-		pilot.forward();
-		do {
-			new_val = light.getNormalizedLightValue();
-			new_color = color(new_val);
-		} while (Math.abs(old_val - new_val) < 30);
-		pilot.stop();
-		correctPosition(pilot, light);
-		pilot.travel(150); // skorygowac aby nie wjezdzac na obiekty!!
-		int oldx = pos_x;
-		int oldy = pos_y;
-		switch (orient) {
-		    case 0: pos_x++;
-		    	    break;
-		    case 1: pos_y++;
-	    	        break;
-		    case 2: pos_x--;
-	    	        break;
-		    case 3: pos_y--;
-	    	        break;
-		};
-		if (mapa.isUnknown(pos_x, pos_y)) {
-	        UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-	        float MAX_DISTANCE = 5.0f; // In centimeters
-	        FeatureDetector fd = new RangeFeatureDetector(us, MAX_DISTANCE, 500);
-	    	Feature result = fd.scan();
-	    	if(result != null) {
-	    		new_val = light.getNormalizedLightValue();
-	    		if (new_val >= RED_L) {
-	    			mapa.setField(pos_x, pos_y, 'H');
-	    		} else {
-	    			mapa.setField(pos_x, pos_y, 'P');
-	    		}
-	    		pilot.travel(-100.0f);
-	    		pos_x = oldx;
-	    		pos_y = oldy;
-	        } else {
-	    		new_color = color(light.getNormalizedLightValue());
-	    		if (new_color == "blue") {
-		        	mapa.setField(pos_x, pos_y, 'N');
-	    		} else if (new_color == "red") {
-		        	mapa.setField(pos_x, pos_y, 'C');
-	    		} else {
-	            	mapa.setField(pos_x, pos_y, 'B');
-	    		}
-	    		pilot.travel(50);
-	        }
-		} else {
-			pilot.travel(50);
-		}
+    private static void setColorValues(LightSensor light) {
+        String kolory[] = {"niebieski", "czerwony", "czarny","bialy"}; 
+        int wartosci[] = new int[4];
+        for (int i=0; i<4; i++) {
+            LCD.clear();
+            LCD.drawString("Pokaz " + kolory[i], 0, 0);
+            LCD.drawString("I nacisnij prawy", 0, 1);
+            while(!Button.RIGHT.isDown()) {
+                int value = light.getNormalizedLightValue();
+                LCD.drawInt(value, 0,2);
+            }
+            while(!Button.RIGHT.isUp()) {}
+            int value = light.getNormalizedLightValue();
+            wartosci[i] = value;
+        }
+        BLUE_L = wartosci[0] - 20;
+        int temp = wartosci[1] - wartosci[0];
+        if (temp < 30){
+            BLUE_H = wartosci[0] + (int)Math.floor(temp/2);
+            RED_L = BLUE_H;
+        } else {
+            BLUE_H = wartosci[0] + 15;
+            RED_L = wartosci[1] - 15;
+        }
+        temp = wartosci[2] - wartosci[1];
+        if (temp < 30){
+            RED_H = wartosci[1] + (int)Math.floor(temp/3);
+            BLACK_L = RED_H;
+        } else {
+            RED_H = wartosci[1] + 15;
+            BLACK_L = wartosci[2] - 15;
+        }
+        temp = wartosci[3] - wartosci[2];
+        if (temp < 30){
+            BLACK_H = wartosci[2] + (int)Math.floor(temp/3);
+            WHITE_L = BLACK_H;
+        } else {
+            BLACK_H = wartosci[2] + 15;
+            WHITE_L = wartosci[3] - 15;
+        }
+        WHITE_H = wartosci[3] + 20;
+    }
+    
+    private static String color(int value) {
+        if ((value > BLUE_L)&&(value <= BLUE_H))
+            return "blue";
+        if ((value > RED_L)&&(value <= RED_H))
+            return "red";
+        if ((value > BLACK_L)&&(value <= BLACK_H))
+            return "black";
+        if ((value > WHITE_L)&&(value <= WHITE_H))
+            return "white";
+        return "unknown";
+    }
+    
+    private static DMap moveOneFront(DifferentialPilot pilot, LightSensor light, DMap mapa) {
+        int new_val = light.getNormalizedLightValue();
+        String start_color = color(new_val);
+        int old_val = new_val;
+        String new_color = start_color;
+        pilot.forward();
+        do {
+            new_val = light.getNormalizedLightValue();
+            new_color = color(new_val);
+        } while ((new_color != start_color) || (new_color == "unknown"));
+        pilot.stop();
+        correctPosition(pilot, light);
+        pilot.travel(150); // skorygowac aby nie wjezdzac na obiekty!!
+        int oldx = pos_x;
+        int oldy = pos_y;
+        switch (orient) {
+            case 0: pos_x++;
+                    break;
+            case 1: pos_y++;
+                    break;
+            case 2: pos_x--;
+                    break;
+            case 3: pos_y--;
+                    break;
+        };
+        if (mapa.isUnknown(pos_x, pos_y)) {
+            new_color = color(light.getNormalizedLightValue());
+            if (new_color == "blue") {
+                mapa.setField(pos_x, pos_y, 'N');
+            } else if (new_color == "red") {
+                mapa.setField(pos_x, pos_y, 'C');
+            } else {
+                mapa.setField(pos_x, pos_y, 'B');
+            }
+            pilot.travel(50);
+        } else {
+            pilot.travel(50);
+        }
         // Rozbić na dwa ruszania się + korygacja + sprawdzenie czy nie ma przeszkody
         return mapa;
-	}
+    }
 
-	private static void correctPosition(DifferentialPilot pilot,
-			LightSensor light) {
-		int start_val = light.getNormalizedLightValue();
-		int now_val = start_val;
-		pilot.rotateLeft();
-		while (Math.abs(start_val - now_val) < 10) {
-			now_val = light.getNormalizedLightValue();
-		}
-		pilot.stop();
-		OdometryPoseProvider pp = new OdometryPoseProvider(pilot);
+    private static void correctPosition(DifferentialPilot pilot,
+            LightSensor light) {
+        int start_val = light.getNormalizedLightValue();
+        int now_val = start_val;
+        pilot.rotateLeft();
+        // TUTAJ ZMIENIAC 1
+        while (Math.abs(start_val - now_val) < 15) {
+            now_val = light.getNormalizedLightValue();
+        }
+        pilot.stop();
+        OdometryPoseProvider pp = new OdometryPoseProvider(pilot);
         Pose pose;
         pilot.rotateRight();
         do {
-        	start_val = light.getNormalizedLightValue();
-        } while (start_val == now_val);
+            start_val = light.getNormalizedLightValue();
+        } while (Math.abs(start_val - now_val) < 3);
+        // TUTAJ ZMIENIAC 2
         pose  = pp.getPose();
-        pilot.rotate(pose.getHeading() / 2.0f);
-	}
+        pilot.rotate(pose.getHeading() / 2.0f); // SPRAWDZIC CZY NA PEWNO NIE UJEMNE!!!
+    }
 
-	private static DMap goAWay(String way, DifferentialPilot pilot, LightSensor light, DMap mapa) {
-		do {
-			char next_l = way.charAt(0);
-			way = way.substring(1);
-			if (next_l == 'r') {
-				pilot.rotate(90.0f);
-				orient = (orient + 1) % 4;
-			} else if (next_l == 'l') {
-				pilot.rotate(-90.0f);
-				orient = (orient - 1);
-				if (orient == -1) {
-					orient = 3;
-				}
-			} else if (next_l == 'f') {
-				mapa = moveOneFront(pilot, light, mapa);
-			}
-		} while (way.length() != 0);
-		return mapa;
-	}
-	
-	public static void main(String[] args) {
-		LightSensor light = new LightSensor(SensorPort.S4);
-	    DifferentialPilot pilot = new DifferentialPilot(56, 128, Motor.B, Motor.C);
-	    pilot.setTravelSpeed(75.0f);
-	    pilot.setRotateSpeed(10.0f);
-		
-		setColorValues(light);
-		
-		LCD.clear();
-
-		int iterator = 0;
-	    /*LCD.clear();
-		LCD.drawString("w " + WHITE_L + " " + WHITE_H, 0, 0);
-		LCD.drawString("r " + RED_L + " " + RED_H, 0, 1);
-		LCD.drawString("b " + BLUE_L + " " + BLUE_H, 0, 2);*/
-	    while(!Button.ENTER.isDown()) {
-		}
-	    
-		DMap mapa = new DMap();
-
-		/*iterator = 0;
-	    while(!Button.ESCAPE.isDown()) {
-			int value = light.getNormalizedLightValue();
-
-			if (iterator % 10 == 0)
-			    LCD.clear();
-        	String color = color(value);
-        	LCD.drawString(color, 0, 1);
-	        LCD.drawInt(value, 0,0);
-	        iterator++;
-		}*/
-		
-		mapa = moveOneFront(pilot, light, mapa);
-		mapa.setField(pos_x, pos_y, 'S');
-
-		while (mapa.haveUnknown()){
-            String way = mapa.findAWay(pos_x, pos_y); // W odległości manhatańskiej/taksówkowej
-        	LCD.drawString(way, 0, 4);
-            mapa = goAWay(way, pilot, light, mapa);
-		}
-
-		mapa.printOnDisplay();
+    private static DMap goAWay(String way, DifferentialPilot pilot, LightSensor light, DMap mapa) {
+        do {
+            char next_l = way.charAt(0);
+            way = way.substring(1);
+            // TODO: Check ROTATE!
+            if (next_l == 'r') {
+                pilot.rotate(90.0f);
+                orient = (orient + 1) % 4;
+            } else if (next_l == 'l') {
+                pilot.rotate(-90.0f);
+                orient = (orient - 1);
+                if (orient == -1) {
+                    orient = 3;
+                }
+            } else if (next_l == 'f') {
+                mapa = moveOneFront(pilot, light, mapa);
+            }
+        } while (way.length() != 0);
+        return mapa;
+    }
+    
+    public static void main(String[] args) {
+        LightSensor light = new LightSensor(SensorPort.S4);
+        DifferentialPilot pilot = new DifferentialPilot(56, 128, Motor.B, Motor.C);
+        pilot.setTravelSpeed(75.0f);
+        pilot.setRotateSpeed(10.0f);
         
-		while(!Button.RIGHT.isDown()) {
-		}
+        setColorValues(light);
+        
+        LCD.clear();
 
-	}
+        int iterator = 0;
+        /*LCD.clear();
+        LCD.drawString("w " + WHITE_L + " " + WHITE_H, 0, 0);
+        LCD.drawString("r " + RED_L + " " + RED_H, 0, 1);
+        LCD.drawString("b " + BLUE_L + " " + BLUE_H, 0, 2);
+        LCD.drawString("k " + BLACK_L + " " + BLACK_H, 0, 3);*/
+        while(!Button.ENTER.isDown()) {
+        }
+        
+        DMap mapa = new DMap();
+
+        /*iterator = 0;
+        while(!Button.ESCAPE.isDown()) {
+            int value = light.getNormalizedLightValue();
+
+            if (iterator % 10 == 0)
+                LCD.clear();
+            String color = color(value);
+            LCD.drawString(color, 0, 1);
+            LCD.drawInt(value, 0,0);
+            iterator++;
+        }*/
+        
+        mapa = moveOneFront(pilot, light, mapa);
+        mapa.setField(pos_x, pos_y, 'S');
+
+        while (mapa.haveUnknown()){
+            String way = mapa.findAWay(pos_x, pos_y, orient); // W odległości manhatańskiej/taksówkowej
+            //LCD.drawString(way, 0, 4);
+            mapa = goAWay(way, pilot, light, mapa);
+        }
+
+        mapa.printOnDisplay();
+
+        Sound.beep();
+        
+        while(!Button.RIGHT.isDown()) {
+        }
+
+    }
 
 }
